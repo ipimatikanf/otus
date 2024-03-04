42a5a4f950d6e67f8c13572a098187b1d5d2cc99
ipmatikanf <n.filatov@ipmatika.com>
2024-03-04 14:39:01 +0300

    ДЗ 2

---
+++ new.py
@@ -0,0 +1,21 @@
+def share_bread(N, K):
+    x = round(K // N)
+    y = K - N * x
+    return x, y
+
+# если в функции всё верно, то после выполнения этой строчки, не должно выскакивать ошибок
+assert share_bread(N=3, K=14) == (4, 2)
+
+
+def leap_year(year):
+    if year % 400 == 0:
+        text_result = 'YOU SHALL PASS'
+    elif year % 100 == 0:
+        text_result = 'YOU SHALL NOT PASS'
+    elif year % 4 == 0:
+        text_result = 'YOU SHALL PASS'
+    else:
+        text_result = 'YOU SHALL NOT PASS'
+    return text_result
+
+assert leap_year(5) == 'YOU SHALL NOT PASS'
