ae04d6f456e1e5391edcad1523a66f9f585abc02
ipmatikanf <n.filatov@ipmatika.com>
2024-03-05 16:02:11 +0300

    Все ДЗ 1

--- new.py
+++ new.py
@@ -1,26 +1,26 @@
-
-def share_bread(N, K):
-    x = round(K // N)
-    y = K - N * x
-    return x, y
-
-# если в функции всё верно, то после выполнения этой строчки, не должно выскакивать ошибок
-assert share_bread(N=3, K=14) == (4, 2)
+import numpy as np
 
 
-def leap_year(year):
-    if year % 400 == 0:
-        text_result = 'YOU SHALL PASS'
-    elif year % 100 == 0:
-        text_result = 'YOU SHALL NOT PASS'
-    elif year % 4 == 0:
-        text_result = 'YOU SHALL PASS'
-    else:
-        text_result = 'YOU SHALL NOT PASS'
-    return text_result
+def cal_euclidean(a, b):
+    square = np.square(a - b)
+    sum_square = np.sum(square)
+    distance = np.sqrt(sum_square)
+    return distance
 
-assert leap_year(5) == 'YOU SHALL NOT PASS'
+def cal_manhattan(a, b):
+    modul = np.abs(a - b)
+    distance = np.sum(modul)
+    return distance
 
+def cal_cosine(a, b):
+    umno = np.dot(a, b)
+    n1 = np.linalg.norm(a)
+    n2 = np.linalg.norm(b)
+    distance = 1 - (umno / (n1 * n2))
+    return distance
 
 a = np.random.randint(-10, 10, size=10)
-print(a)
+b = np.random.randint(-10, 10, size=10)
+print(cal_euclidean(a, b))
+print(cal_manhattan(a, b))
+print(cal_cosine(a, b))
